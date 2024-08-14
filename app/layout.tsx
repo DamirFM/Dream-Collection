82af7d5e24d1dd2db69accf7777cd7d388b9f0be
DamirFM <yakupovdamir93@gmail.com>
2024-08-14 13:17:28 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import { NextAuthProvider } from "./providers";
 
 const nunito = Nunito({ subsets: ["latin"] });
