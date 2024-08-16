8785fd99920705b4feca23cc8c0d19e3f7d65c06
DamirFM <yakupovdamir93@gmail.com>
2024-08-16 19:04:57 -0400

    update posts/route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,29 +1,43 @@
-import connectMongoDB from "@/lib/mongodb";
-import Post from "@/models/post";
-import { NextResponse, NextRequest } from "next/server";
-import { applyCors } from '@/lib/cors';
+import connectMongoDB from '@/lib/mongodb';
+import Post from '@/models/post';
+import { NextRequest, NextResponse } from 'next/server';
 
+// Function to handle CORS headers
+function handleCors(response: NextResponse) {
+  response.headers.set('Access-Control-Allow-Origin', '*'); // Adjust this as needed for security
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+}
+
+// Handle POST requests
 export async function POST(request: NextRequest) {
-  applyCors(request, NextResponse, () => {});
+  let response = NextResponse.json({ message: 'Post Created' }, { status: 201 });
+  handleCors(response);
 
   const { title, description } = await request.json();
-  console.log(title, description);
   await connectMongoDB();
   await Post.create({ title, description });
-  return NextResponse.json({ message: "Post Created" }, { status: 201 });
+  return response;
 }
 
+// Handle GET requests
 export async function GET(request: NextRequest) {
-  applyCors(request, NextResponse, () => {});
+  let response = NextResponse.json({ posts: [] });
+  handleCors(response);
+
   await connectMongoDB();
   const posts = await Post.find();
-  return NextResponse.json({ posts });
+  response = NextResponse.json({ posts });
+  return response;
 }
 
+// Handle DELETE requests
 export async function DELETE(request: NextRequest) {
-  applyCors(request, NextResponse, () => {});
+  let response = NextResponse.json({ message: 'Post deleted' }, { status: 200 });
+  handleCors(response);
+
   const id = request.nextUrl.searchParams.get('id');
   await connectMongoDB();
   await Post.findByIdAndDelete(id);
-  return NextResponse.json({ message: "Post deleted" }, { status: 200 });
+  return response;
 }
