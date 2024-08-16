95228ee5f799c26bac8b1ec3699817874245a3e8
DamirFM <yakupovdamir93@gmail.com>
2024-08-16 19:07:55 -0400

    fix

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,42 +2,53 @@ import connectMongoDB from '@/lib/mongodb';
 import Post from '@/models/post';
 import { NextRequest, NextResponse } from 'next/server';
 
-// Function to handle CORS headers
 function handleCors(response: NextResponse) {
-  response.headers.set('Access-Control-Allow-Origin', '*'); // Adjust this as needed for security
+  response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
 }
 
+interface Post {
+  _id: string;
+  title: string;
+  description: string;
+}
+
+interface PostResponse {
+  posts: Post[];
+}
+
+interface MessageResponse {
+  message: string;
+}
 // Handle POST requests
 export async function POST(request: NextRequest) {
-  let response = NextResponse.json({ message: 'Post Created' }, { status: 201 });
-  handleCors(response);
-
   const { title, description } = await request.json();
   await connectMongoDB();
   await Post.create({ title, description });
+
+  let response = NextResponse.json<MessageResponse>({ message: 'Post Created' }, { status: 201 });
+  handleCors(response);
   return response;
 }
 
 // Handle GET requests
 export async function GET(request: NextRequest) {
-  let response = NextResponse.json({ posts: [] });
-  handleCors(response);
-
   await connectMongoDB();
   const posts = await Post.find();
-  response = NextResponse.json({ posts });
+
+  let response = NextResponse.json<PostResponse>({ posts });
+  handleCors(response);
   return response;
 }
 
 // Handle DELETE requests
 export async function DELETE(request: NextRequest) {
-  let response = NextResponse.json({ message: 'Post deleted' }, { status: 200 });
-  handleCors(response);
-
   const id = request.nextUrl.searchParams.get('id');
   await connectMongoDB();
   await Post.findByIdAndDelete(id);
+
+  let response = NextResponse.json<MessageResponse>({ message: 'Post deleted' }, { status: 200 });
+  handleCors(response);
   return response;
 }
