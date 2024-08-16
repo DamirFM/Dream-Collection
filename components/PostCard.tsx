a872df793044852a594f90ba956a75a5a5b5f253
DamirFM <yakupovdamir93@gmail.com>
2024-08-16 13:58:58 -0400

    added bg-opacity for Header component

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,26 +3,10 @@ import Link from 'next/link';
 import React, { useRef, useEffect, useState, } from 'react';
 import { HiPencilAlt } from 'react-icons/hi';
 import RemoveBtn from './UI/removeBtn';
-import { useScroll, useTransform, motion } from 'framer-motion'
+// import { useScroll, useTransform, motion } from 'framer-motion'
 
 
 const PostCard = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    // first we need actual HTML reference to the element
-    target: ref,
-    // then we need to define the viewport offset
-    // this mean when we actually will start the animation
-    // start animation "0 1" mean when the bottom of the element is at the top of the viewport
-    // finish animation "1.33 1" mean when the bottom of the element is at the bottom of the viewport
-    offset: ["0 1", "1.33 1"]
-  })
-  // managing the scale and opacity of the element (I do not want section appear from 0 scale and 0 opacity to 1 scale and 1 opacity, I want to start from 0.8 scale and 0.6 opacity to 1 scale and 1 opacity)
-  // we need to use useTransform hook to transform the scrollYProgress
-  // we need to define the range of the scrollYProgress
-  // we need to define the range of the scaleProgess and opacityProgess
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1])
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
   const [posts, setPosts] = useState([]);
   useEffect(() => {
@@ -50,9 +34,8 @@ const PostCard = () => {
 
   return (
     <>
-      <motion.div
-        ref={ref}
-        style={{ scale: scaleProgess, opacity: opacityProgess }}
+      <div
+
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((post: {
           [x: string]: any; title: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined;
@@ -71,7 +54,7 @@ const PostCard = () => {
             </div>
           </div>
         ))}
-      </motion.div>
+      </div>
     </>
   );
 };
