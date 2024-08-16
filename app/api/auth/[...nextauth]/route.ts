f0078360511b3204bd790106e0c6359df561f219
DamirFM <yakupovdamir93@gmail.com>
2024-08-16 14:56:55 -0400

    auth issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
     throw new Error("Missing Google client ID or client secret in environment variables.");
 }
 
-export const authOptions: AuthOptions = {
+ const authOptions: AuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
