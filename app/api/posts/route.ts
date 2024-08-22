74fce1d82a20feffcc486da7e3332aa3c3ae8a8d
DamirFM <yakupovdamir93@gmail.com>
2024-08-21 22:11:36 -0400

    updated posts/route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,11 +2,11 @@ import connectMongoDB from '@/lib/mongodb';
 import Post from '@/models/post';
 import { NextRequest, NextResponse } from 'next/server';
 
-function handleCors(response: NextResponse) {
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-}
+// function handleCors(response: NextResponse) {
+//   response.headers.set('Access-Control-Allow-Origin', '*');
+//   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+// }
 
 interface Post {
   _id: string;
@@ -28,7 +28,7 @@ export async function POST(request: NextRequest) {
   await Post.create({ title, description });
 
   let response = NextResponse.json<MessageResponse>({ message: 'Post Created' }, { status: 201 });
-  handleCors(response);
+  // handleCors(response);
   return response;
 }
 
@@ -38,17 +38,14 @@ export async function GET(request: NextRequest) {
   const posts = await Post.find();
 
   let response = NextResponse.json<PostResponse>({ posts });
-  handleCors(response);
+  // handleCors(response);
   return response;
 }
 
 // Handle DELETE requests
 export async function DELETE(request: NextRequest) {
-  const id = request.nextUrl.searchParams.get('id');
+  const id = request.nextUrl.searchParams.get("id");
   await connectMongoDB();
   await Post.findByIdAndDelete(id);
-
-  let response = NextResponse.json<MessageResponse>({ message: 'Post deleted' }, { status: 200 });
-  handleCors(response);
-  return response;
+  return NextResponse.json({ message: "Post deleted" }, { status: 200 });
 }
