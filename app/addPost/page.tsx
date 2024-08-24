c5cf6fdb11a9e753591a1b81ebd2298f70a4dd0b
DamirFM <yakupovdamir93@gmail.com>
2024-08-23 20:25:49 -0400

    fix handleImageChange function at addPost page

--- app/addPost/page.tsx
+++ app/addPost/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import LoginPage from '../login/page';
@@ -8,18 +7,21 @@ import LoginPage from '../login/page';
 export default function AddPost() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const router = useRouter();
   const { status } = useSession();
   if (status === 'unauthenticated') {
     return <LoginPage />;
   }
-  const handleImageChange = (e) => {
-    setFile(e.target.files[0]); // Store the selected image
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]); // Store the selected image
+    }
   };
 
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!title || !description || !file) {
@@ -33,12 +35,8 @@ export default function AddPost() {
       formData.append('description', description);
       formData.append('file', file); // Append the image to the form data
 
-
       const res = await fetch('/api/posts', {
         method: 'POST',
-        // headers: {
-        //   'Content-Type': 'application/json'
-        // },
         body: formData,
       });
 
@@ -47,13 +45,10 @@ export default function AddPost() {
       } else {
         throw new Error(res.statusText);
       }
-      // setTitle('');
-      // setDescription('');
     } catch (error) {
       console.error('Failed to create post:', error);
     }
-
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col w-full gap-3 px-8 py-2 items-center justify-center h-screen bg-stone-100">
@@ -81,5 +76,5 @@ export default function AddPost() {
         Submit
       </button>
     </form>
-  )
+  );
 }
